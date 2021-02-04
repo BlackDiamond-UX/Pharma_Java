5c4e320a5060a8b771a41dc6add2ec6e21493311
Rouiha Ayoub <58300988+BlackDiamond-UX@users.noreply.github.com>
2021-02-04 20:17:41 +0100

    update and finish work

---
+++ src/com/youcode/pharmajava/model/Pharma.java
@@ -0,0 +1,57 @@
+package com.youcode.pharmajava.model;
+
+public class Pharma {
+	protected int id;
+	protected String item;
+	protected String company;
+	protected String price;
+
+	
+	public Pharma(String item, String company, String price) {
+		super();
+		this.item = item;
+		this.company = company;
+		this.price = price;
+	}
+
+	public Pharma(int id, String item, String company, String price) {
+		super();
+		this.id = id;
+		this.item = item;
+		this.company = company;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
